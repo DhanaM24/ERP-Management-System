eedeec1cc862bfd0a7ad5d1b8b24fe2bf20a4cff
DhanaM24 <dhananjipallegedara432@gmail.com>
2026-08-11 11:44:58 +0530

    create backend files

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express=require("express");
+const cors=require("cors");
+require("dotenv").config();
+
+const connectDB=require("./config/db");
+
+
+const app=express();
+
+
+app.use(cors());
+
+app.use(express.json());
+
+
+connectDB();
+
+
+app.get("/",(req,res)=>{
+
+res.send("ERP API Running");
+
+});
+
+
+app.listen(process.env.PORT,()=>{
+
+console.log("Server Started");
+
+});
